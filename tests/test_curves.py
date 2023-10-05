84fe8cfdfdcfe878d4d0b458b3d78fcd7b2e4f34
Alexander Clark <alexanderthclark@gmail.com>
2023-10-05 16:44:10 -0400

    convert indents to spaces

--- tests/test_curves.py
+++ tests/test_curves.py
@@ -3,17 +3,17 @@ from microecon.curves import Demand, Supply
 
 class TestDemand(unittest.TestCase):
 
-	def setUp(self):
-		self.slope = -2
-		self.intercept = 12
-		self.demand = Demand(self.intercept, self.slope)
+    def setUp(self):
+        self.slope = -2
+        self.intercept = 12
+        self.demand = Demand(self.intercept, self.slope)
 
-	def test_q_intercept(self):
-		self.assertTrue(self.demand.q_intercept == 6)
+    def test_q_intercept(self):
+        self.assertTrue(self.demand.q_intercept == 6)
 
-	# for intentional failure
-	#def test_break(self):
-	#	self.assertTrue(False)
+    # for intentional failure
+    #def test_break(self):
+    #   self.assertTrue(False)
 
-	def tearDown(self):
-		pass
+    def tearDown(self):
+        pass
