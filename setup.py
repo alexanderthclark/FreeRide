8a7fa21f0406c58a68e86c10e70f2937f7aa9aa5
Alexander Clark <alexanderthclark@gmail.com>
2023-10-05 15:24:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 import setuptools
-setuptools.setup(name='microecon',
-version='0.0.1dev',
-description='A package for undergraduate microeconomics.',
-url='#',
-author='Alexander Clark',
-install_requires=['matplotlib','numpy'],
-author_email='',
-packages=setuptools.find_packages(),
-zip_safe=False)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+def get_version():
+    with open("microecon/__init__.py", "r") as f:
+        for line in f:
+            if line.startswith("__version__"):
+                return eval(line.split("=")[-1].strip())
+
+setuptools.setup(
+    name='microecon',
+    version=get_version(),
+	description='A package for undergraduate microeconomics.',
+    long_description=long_description,  # This is the new line
+    long_description_content_type="text/markdown",  # This is the new line
+    url='https://github.com/alexanderthclark/Intro-Microeconomics',
+    author='Alexander Clark',
+    install_requires=['matplotlib','numpy'],
+    author_email='',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
