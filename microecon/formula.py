6d667a6993fda4135cbf0ea449e111d716e4233d
Alexander Clark <alexanderthclark@gmail.com>
2023-10-07 22:34:57 -0400

    formula module

---
+++ microecon/formula.py
@@ -0,0 +1,60 @@
+'''
+Formula module using sympy
+'''
+from microecon.curves import Affine
+
+
+def formula(equation: str):
+    """
+    Parse a linear equation string to extract the slope and intercept, and return an Affine object.
+
+    The function supports linear equations in the following formats:
+    - 'y = mx + b'
+    - 'y = b + mx'
+    - 'x = my + b'
+    - 'x = b + my'
+    with optional whitespaces and optional signs for m and b.
+    The variables 'y' and 'x' can also be represented as 'p', 'P', 'q', or 'Q', respectively.
+
+    Parameters:
+    equation (str): A string representing a linear equation.
+
+    Returns:
+    Affine: An object representing the equation.
+
+    Raises:
+    ValueError: If the input string is not in a valid equation format.
+    			If there is a fraction instead of decimal coefficients or a zero slope.
+
+    Examples:
+    >>> formula('y = 10 - 2x')
+    Affine(intercept=10.0, slope=-2.0)
+    >>> formula('y = 10 - 2*x')
+    Affine(intercept=10.0, slope=-2.0)
+    >>> formula('P=3+0.5Q')
+    Affine(intercept=3.0, slope=0.5)
+    """
+    # Remove whitespaces and equate p with y and q with x
+    equation = (equation.lower()
+                       .replace("p", "y")
+                       .replace("q", "x")
+                       .replace(" ", ""))
+
+    if '/' in equation:
+    	raise ValueError("Unexpected character '/'. Use decimals and not fractions.")
+    
+    # Check if equation is in form of y = mx + b or y = b + mx
+    match = re.match(r"y=(-?\d*\.?\d*)\*?x([+-]\d+\.?\d*)?|y=([+-]?\d+\.?\d*)([+-]\d*\.?\d*)\*?x", equation)
+    if match:
+        slope = float(match.group(1) or match.group(4) or '1')  # Default slope is 1 if not specified
+        intercept = float(match.group(2) or match.group(3) or '0')  # Default intercept is 0 if not specified
+        return Affine(intercept, slope)
+
+    # Check if equation is in form of x = my + b or x = b + my
+    match = re.match(r"x=(-?\d*\.?\d*)\*?y([+-]\d+\.?\d*)?|x=([+-]?\d+\.?\d*)([+-]\d*\.?\d*)\*?y", equation)
+    if match:
+        slope = float(match.group(1) or match.group(4) or '1')  # Default slope is 1 if not specified
+        intercept = float(match.group(2) or match.group(3) or '0')  # Default intercept is 0 if not specified
+        return Affine(intercept, 1 / slope)  # Inverting the slope for this case
+
+    raise ValueError("Invalid equation")
