c25776a81ef4871e86335d9208ee09df92363b6a
Alexander Clark <alexanderthclark@users.noreply.github.com>
2024-01-27 22:37:03 -0500

    Update test_formula.py

--- tests/test_formula.py
+++ tests/test_formula.py
@@ -1,5 +1,5 @@
 import unittest
-from microecon.formula import formula
+from microecon.formula import _formula
 
 class TestFormula(unittest.TestCase):
 
@@ -7,14 +7,14 @@ class TestFormula(unittest.TestCase):
         s1 = 'P = 10 - 2*Q'
         s2 = 'P = 10 - 2Q'
         s3 = 'p = 2 + 4*q'
-        self.affine1 = formula(s1)
-        self.affine2 = formula(s2)
-        self.affine3 = formula(s3)
+        self.affine1 = _formula(s1)
+        self.affine2 = _formula(s2)
+        self.affine3 = _formula(s3)
 
     def test_slope(self):
 
-        self.assertTrue(self.affine1.slope == self.affine2.slope == [-2])
-        self.assertTrue(self.affine3.slope == [4])
+        self.assertTrue(self.affine1[1] == self.affine2[1] == -2)
+        self.assertTrue(self.affine3[1] == 4)
 
     def tearDown(self):
         pass
