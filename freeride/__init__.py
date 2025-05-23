1814dec90be9722a6c2d90452fd05074f6b0e757
Alexander Clark <alexanderthclark@users.noreply.github.com>
2025-05-23 16:41:56 -0700

    feat: expose main classes in package init

--- freeride/__init__.py
+++ freeride/__init__.py
@@ -1,2 +1,12 @@
 # FreeRide
 __version__ = '0.0.7'
+
+from .curves import Demand, Supply
+from .equilibrium import Equilibrium, Market
+
+__all__ = [
+    "Demand",
+    "Supply",
+    "Equilibrium",
+    "Market",
+]
